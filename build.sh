6b57e24af5698d8b246aca16ed76be79bdc3c046
rf <rf@computer>
2026-07-29 19:02:36 -0400

    added red_random rng module

--- build.sh
+++ build.sh
@@ -8,5 +8,7 @@ mkdir -p build/staging
 
 # Converts fypp sources to f90
 uvx fypp src/templates/red_vector.fypp build/staging/red_vector.f90
+uvx fypp src/templates/red_random.fypp build/staging/red_random.f90
 
 
+cat build/staging/red_vector.f90 build/staging/red_random.f90 > build/redstart.f90
